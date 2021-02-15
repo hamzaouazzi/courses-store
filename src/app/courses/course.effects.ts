69921c65a44748a1bfc68e96d19eeee03861c0ad
hamzaouazzi <ouazzihamza97@gmail.com>
2021-02-15 11:34:18 +0100

    added course effects

---
+++ src/app/courses/course.effects.ts
@@ -0,0 +1,66 @@
+import {Injectable} from '@angular/core';
+import {Actions, createEffect, ofType} from '@ngrx/effects';
+import {
+  AllCoursesLoaded,
+  AllCoursesRequested,
+  CourseActionTypes,
+  CourseLoaded,
+  CourseRequested, LessonsPageCancelled, LessonsPageLoaded,
+  LessonsPageRequested
+} from './course.actions';
+import {throwError,of} from 'rxjs';
+import {catchError, concatMap, exhaustMap, filter, map, mergeMap, withLatestFrom} from "rxjs/operators";
+import {CoursesService} from './services/courses.service';
+import {AppState} from '../reducers';
+import {select, Store} from '@ngrx/store';
+import {allCoursesLoaded} from './course.selectors';
+
+@Injectable()
+export class CourseEffects {
+
+
+  loadCourse$ = createEffect(() => this.actions$
+  .pipe(
+    ofType<CourseRequested>(CourseActionTypes.CourseRequested),
+    mergeMap(action => this.coursesService.findCourseById(action.payload.courseId)),
+    map(course => new CourseLoaded({course}))
+  )
+  );
+
+
+  loadAllCourses$ = createEffect(() => this.actions$
+  .pipe(
+    ofType<AllCoursesRequested>(CourseActionTypes.AllCoursesRequested),
+    withLatestFrom(this.store.pipe(select(allCoursesLoaded))),
+    filter(([action, allCoursesLoaded]) => !allCoursesLoaded),
+    mergeMap(() => this.coursesService.findAllCourses()),
+    map(courses => new AllCoursesLoaded({courses}))
+  )
+  );
+
+
+  loadLessonsPage$ = createEffect(() => this.actions$
+  .pipe(
+    ofType<LessonsPageRequested>(CourseActionTypes.LessonsPageRequested),
+    mergeMap(({payload}) =>
+            this.coursesService.findLessons(payload.courseId,
+                        payload.page.pageIndex, payload.page.pageSize)
+              .pipe(
+                catchError(err => {
+                  console.log('error loading a lessons page ', err);
+                  this.store.dispatch(new LessonsPageCancelled());
+                  return of([]);
+                })
+              )
+    ),
+    map(lessons => new LessonsPageLoaded({lessons}))
+   )
+  ) ;
+
+
+    constructor(private actions$ :Actions, private coursesService: CoursesService,
+      private store: Store<AppState>) {
+
+}
+
+}
