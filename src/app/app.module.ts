58c17d70f10b20361b7686ee0e94b6df047169e3
hamzaouazzi <ouazzihamza97@gmail.com>
2021-02-16 09:27:52 +0100

    added course list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { CourseComponent } from './courses/course/course.component';
 import { CourseDialogComponent } from './courses/course-dialog/course-dialog.component';
+import { CourseCardListComponent } from './courses/course-card-list/course-card-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     CourseComponent,
-    CourseDialogComponent
+    CourseDialogComponent,
+    CourseCardListComponent
   ],
   imports: [
     BrowserModule
